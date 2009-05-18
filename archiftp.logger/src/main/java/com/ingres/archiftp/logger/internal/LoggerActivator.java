0c53aad84ea8745f374384e54cd89bc04ac4d5a2
Jatesadakarn Seangrat <champ.jss@gmail.com>
2009-05-18 12:03:53 +0700

    New bundle archiftp.logger, and use this to log. Duplicated codes are removed.

---
+++ archiftp.logger/src/main/java/com/ingres/archiftp/logger/internal/LoggerActivator.java
@@ -0,0 +1,28 @@
+package com.ingres.archiftp.logger.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.log.LogService;
+import org.osgi.util.tracker.ServiceTracker;
+
+import com.ingres.archiftp.logger.Logger;
+
+public final class LoggerActivator implements BundleActivator {
+	
+	private ServiceTracker logServiceTracker;
+	
+	public void start(BundleContext bc) throws Exception {
+		this.logServiceTracker = getServiceTracker(LogService.class, bc);
+		this.logServiceTracker.open();
+		Logger.setTracker(this.logServiceTracker);
+	}
+
+	public void stop(BundleContext bc) throws Exception {
+		this.logServiceTracker.close();
+	}
+	
+	@SuppressWarnings("unchecked")
+	private ServiceTracker getServiceTracker(Class service, BundleContext bc) {
+		return new ServiceTracker(bc, service.getName(), null);
+	}
+}
